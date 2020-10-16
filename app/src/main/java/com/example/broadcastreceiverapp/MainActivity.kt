e7936961ba207de6e5510426770042f6f29e9649
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-10-17 00:01:55 +0700

    add download file with background service

--- app/src/main/java/com/example/broadcastreceiverapp/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.broadcastreceiverapp
 
 import android.Manifest
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.content.pm.PermissionInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -14,8 +19,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
         private const val SMS_REQUEST_CODE = 101
     }
+
+    private lateinit var downloadReceiver: BroadcastReceiver
 
     object PermissionManager {
         fun check(activity: Activity, permissions: String, requestCode: Int) {
@@ -30,12 +38,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         btn_permission.setOnClickListener(this)
+        btn_download.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(p0: Context, p1: Intent?) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+                Toast.makeText(p0, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
+
+
 
     override fun onClick(p0: View) {
         when {
             p0.id == R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+            p0.id == R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(downloadReceiver)
     }
 
     override fun onRequestPermissionsResult(
